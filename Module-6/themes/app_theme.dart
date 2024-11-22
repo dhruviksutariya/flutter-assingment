469ccaf6d67c059303b1ec8b067175ac29c4e27d
Dhruvik Sutariya <142383055+dhruviksutariya@users.noreply.github.com>
2024-11-22 14:55:30 +0530

    Delete Module-6 directory

--- Module-6/themes/app_theme.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import '../constant/constants.dart';
-
-ThemeData AppTheme(BuildContext context) {
-  return ThemeData(scaffoldBackgroundColor: AppConstant.bgColor);
-}
